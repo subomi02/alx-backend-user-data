b2ed462c3ad876d9f71e937ac01f85c35ebc9424
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-06-06 01:42:41 +0100

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""The `user` model's module.
+"""Adetunji Olasubomi
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    """Represents a record from the `user` table.
+    """
     """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
